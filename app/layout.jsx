89a4d379f4e0aaa71b22562387ab63e85bc7b0a6
Dawodu Bolaji <dawodubolaji189@gmail.com>
2023-11-01 23:02:23 +0100

    new

--- app/layout.jsx
+++ app/layout.jsx
@@ -70,7 +70,7 @@ export default function RootLayout({ children }) {
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
-            })(); 
+            })();
         `}
       </script>
         </body>
