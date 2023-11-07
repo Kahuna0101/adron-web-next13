f3bfab0391ae7ce2f38f9c4a0e3241fbea1b5d6f
Dawodu Bolaji <dawodubolaji189@gmail.com>
2023-11-07 23:30:57 +0100

    new modifications

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
     default: "Adron Homes & Properties",
   },
   description:
-    "Adron Homes and Properties is a leading real estate development company committed to providing exceptional and affordable housing solutions. With a focus on innovation, quality, and customer satisfaction, Adron Homes has established itself as a trusted name in the real estate industry. Our diverse portfolio includes residential estates, commercial properties, and luxury developments designed to meet the unique needs of our clients. We pride ourselves on creating communities that offer a perfect blend of modern amenities, green spaces, and a high standard of living. At Adron Homes, we believe in the power of home to transform lives, and our dedicated team works tirelessly to turn that belief into reality. Whether you are a first-time homebuyer, an investor, or seeking a dream home, Adron Homes and Properties is your partner in building a better future. Explore our range of properties, experience excellence in real estate, and let us help you find the home that suits your lifestyle. Welcome to Adron Homes and Properties – where dreams come home.",
+    "Adron Homes and Properties is a leading real estate development company committed to providing exceptional and affordable housing solutions. With a focus on innovation, quality, and customer satisfaction.",
   keywords: [
     "properties",
     "sale",
@@ -19,6 +19,7 @@ export const metadata = {
     "intelligent",
     "aderonke",
     "olori",
+    "estates",
   ],
   robots: {
     index: false,
